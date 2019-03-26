89d7c25e47164bc015d7180cee733756c4eb0c49
Kirin Patel <kirinpatel@gmail.com>
2019-03-26 16:38:37 -0400

    Added docs folder

---
+++ docs/precache-manifest.c5382fee1b87db128d1ee82d5ad907b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a0696f1f9f9a7d3c61a",
+    "url": "/static/js/main.ec7616e8.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b89d155389d9199934c2",
+    "url": "/static/css/2.90a6f61e.chunk.css"
+  },
+  {
+    "revision": "b89d155389d9199934c2",
+    "url": "/static/js/2.48bd4ef8.chunk.js"
+  },
+  {
+    "revision": "f0b8894d9785e3e73d1566a5243252d6",
+    "url": "/index.html"
+  }
+];
